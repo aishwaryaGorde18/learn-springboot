510d208ff15397ad3eefb14a6b51aec836686e89
aishwaryaGorde18 <aishwaryabgorde1805@gmail.com>
2025-07-10 10:59:43 +0530

    initial commit

---
+++ springbootApplication-project/src/main/java/com/innoxIT/springbootApplication_project/SpringbootApplicationProjectApplication.java
@@ -0,0 +1,14 @@
+package com.innoxIT.springbootApplication_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplicationProjectApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootApplicationProjectApplication.class, args);
+	}
+
+}
