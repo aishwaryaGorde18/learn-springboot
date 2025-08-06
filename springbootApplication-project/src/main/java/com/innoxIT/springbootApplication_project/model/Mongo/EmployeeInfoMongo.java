fadf831984fe310e2e98e68cb1bbb1ef5d4fa2eb
aishwaryaGorde18 <aishwaryabgorde1805@gmail.com>
2025-08-06 14:17:05 +0530

    feat: add actuator endpoints and Swagger UI for monitoring and API documentation

--- springbootApplication-project/src/main/java/com/innoxIT/springbootApplication_project/model/Mongo/EmployeeInfoMongo.java
+++ springbootApplication-project/src/main/java/com/innoxIT/springbootApplication_project/model/Mongo/EmployeeInfoMongo.java
@@ -1,8 +1,13 @@
 package com.innoxIT.springbootApplication_project.model.Mongo;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
 
 @Document(collection = "employees")
 @Data
@@ -13,4 +18,10 @@ public class EmployeeInfoMongo {
     public String name;
     public String department;
     public Double salary;
+
+    @CreatedDate
+    private Date createdAt;
+
+    @LastModifiedDate
+    private Date updatedAt;
 }
